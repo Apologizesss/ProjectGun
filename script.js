9de746248e09a5cf58b533705d3b81dcbea073ed
Warapob <gunthorn.siri@bu.ac.th>
2023-10-31 20:51:17 +0700

    Recent by Warapob: 	deleted:    GunMain.htm 	modified:   Test2.htm 	new file:   images/BG1.png 	new file:   images/BG2.1.png 	new file:   images/BG2.png 	new file:   images/BG2_1.png     new file:   images/BG2_2.png 	new file:   images/BG2_3.png 	new file:   images/BG2_4.png 	new file:   images/BG3_1.png 	modified:   script.js 	modified:   styles.css 	deleted:    styless.css 	deleted:    stylesss.css 	deleted:    stylesss1.css 	deleted:    test.htm

--- script.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.hamburger').addEventListener('click', function() {
-    const navItems = document.querySelector('.nav-items');
-    if(navItems.style.display === 'none' || navItems.style.display === '') {
-        navItems.style.display = 'flex';
-    } else {
-        navItems.style.display = 'none';
-    }
-});
